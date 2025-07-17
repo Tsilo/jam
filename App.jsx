5250831196aea47d542206b902393f3cb4f26d69
Zurab <fortsilo@gmail.com>
2025-07-17 23:00:04 +0400

    react init

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+	return (
+		<div className="p-6 text-center mt-20 h-full">
+			zd reload
+			<h1 className="text-3xl font-bold">Hello from React + Tailwind v4 + Pear!</h1>
+		</div>
+	)
+}
